45dc38963043100510e0a3824368057b9f8bfa61
Ygor-Andrade <111753698+Ygor-Andrade@users.noreply.github.com>
2022-09-12 11:47:08 -0300

    Update JS

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,56 @@
-//inicializa animações scroll do AOS
- AOS.init();
+/*  ///////////////////  FUNÇÕES   ////////////////////  */
+const aviso = (msg)=> {
+    alert(msg)
+}
+
+const mostraIdade = ()=>{
+    let span = document.getElementById('txt-idade')
+    let campoIdade = document.getElementById('idade')
+    span.innerText = campoIdade.value
+}
+
+
+
+
+
+/* ---------------------------------------------------- */
+
+
+
+
+
+/* //////////// EVENTOS E EXECUÇÕES AUTOMÁTICAS  /////////// */
+
+
+/* aviso('Tenha um bom dia DAVE')
+
+document.getElementById('nome').addEventListener('click', function(){
+    aviso('OLÁ MUNDO!! Este é o planeta terra?')
+}) */
+
+mostraIdade()
+
+//Inicializa animações scroll do AOS
+AOS.init();
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+( ()=> {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
